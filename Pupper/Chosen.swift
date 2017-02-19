c05be2e21c2bdb14cfa0a5cba212ad4e594989bf
Olivia Brown <obrown917@gmail.com>
2017-02-19 09:10:59 -0800

    dog with label and description

---
+++ Pupper/Chosen.swift
@@ -0,0 +1,9 @@
+//
+//  Chosen.swift
+//  Pupper
+//
+//  Created by Olivia on 2/18/17.
+//  Copyright © 2017 Olivia. All rights reserved.
+//
+
+import Foundation
---
+++ Pupper/ChosenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChosenTableViewCell.swift
+//  Pupper
+//
+//  Created by Olivia on 2/19/17.
+//  Copyright © 2017 Olivia. All rights reserved.
+//
+
+import UIKit
+
+class ChosenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
