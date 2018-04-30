cc6f9864281040a33c10cbbf071275ebdab034e1
olivia_r_brown <olivia_r_brown@apple.com>
2018-04-30 12:32:12 -0700

    Swift 4 conversion

--- Pupper/Controller/LocationViewController.swift
+++ Pupper/Controller/LocationViewController.swift
@@ -21,7 +21,7 @@ class LocationViewController: UIViewController {
         
     }
     
-    func dismissKeyboard() {
+    @objc func dismissKeyboard() {
         
         view.endEditing(true)
     }
@@ -38,7 +38,7 @@ class LocationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    func textFieldDidChange(_ textField: UITextField) {
+    @objc func textFieldDidChange(_ textField: UITextField) {
         
         
         let geocoder = CLGeocoder()
