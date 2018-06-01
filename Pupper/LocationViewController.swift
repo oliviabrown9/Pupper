191fdb67468b865dca5d43724bf33f0abd7a22ee
olivia_r_brown <olivia_r_brown@apple.com>
2018-06-01 13:53:38 -0700

    Safer text field verification

--- Pupper/LocationViewController.swift
+++ Pupper/LocationViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import CoreLocation
 
-
 class LocationViewController: UIViewController {
     
     @IBAction func unwindToSelectDog(segue: UIStoryboardSegue) {}
@@ -16,54 +15,40 @@ class LocationViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var locationLabel: UILabel!
     var dogBreed: DogPreference?
-    @IBAction func continueButtonPressed(_ sender: Any) {
-        
-    }
     
     @objc func dismissKeyboard() {
-        
         view.endEditing(true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         
         let tap: UITapGestureRecognizer?
         tap = UITapGestureRecognizer(target: self, action: #selector(LocationViewController.dismissKeyboard))
         view.addGestureRecognizer(tap!)
-        
-        // Do any additional setup after loading the view.
     }
 
     @objc func textFieldDidChange(_ textField: UITextField) {
-        
-        
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(textField.text!) {
             (placemarks, error) -> Void in
-            // Placemarks is an optional array of CLPlacemarks, first item in array is best guess of Address
-            
             if let placemark = placemarks?[0] {
-                
                 let addressDict = placemark.addressDictionary as? [String: Any]
-                let city = addressDict?["City"] as! String
-                let state = addressDict?["State"] as! String
-                
-                if (textField.text?.count)! >= 5{
-                self.locationLabel.text = "\(city), \(state)"
-                self.dogBreed = DogPreference(zipCode: Int(textField.text!)!)
+                if let city = addressDict?["City"], let state = addressDict?["State"] {
+                    if let locationText = textField.text, let locationInt = Int(locationText) {
+                        if locationText.count >= 5 {
+                            self.locationLabel.text = "\(city), \(state)"
+                            self.dogBreed = DogPreference(zipCode: locationInt)
+                        }
+                    }
                 }
             }
-            
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destination = segue.destination as! HomeViewController
+        let destination = segue.destination as! IdealDogViewController
         destination.dogBreed = self.dogBreed
-        
     }
-
 }
